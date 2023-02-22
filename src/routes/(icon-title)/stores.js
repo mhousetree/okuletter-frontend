2b3edde68bb9cee162b4ae0d32523c90137e02f5
mhousetree <mhousetree@gmail.com>
2023-02-22 19:42:55 +0900

    Add store

---
+++ src/routes/(icon-title)/stores.js
@@ -0,0 +1,11 @@
+import { writable } from 'svelte/store';
+
+export const toName = writable('');
+export const fromName = writable('');
+export const text = writable('');
+export const font = writable('');
+export const background = writable('');
+export const pen = writable('');
+export const emvelope = writable('');
+export const stamp = writable('');
+export const backgroundIsDark = writable(false);
