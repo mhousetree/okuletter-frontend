e469556f82a8c835d0f0595108eb393b16950f58
mhousetree <mhousetree@gmail.com>
2023-03-02 17:31:22 +0900

    Add method: "GET"

--- src/routes/(icon)/letters/[key]/+page.js
+++ src/routes/(icon)/letters/[key]/+page.js
@@ -1,6 +1,7 @@
 export async function load({ params }) {
 	const response = await fetch(`https://okltrbackend-1-q1172794.deta.app/letters/${params.key}`
 	, {
+		method: "GET",
 		headers: {
 			"X-Space-App-Key": 'HprcHEzprBLrjN8xu6Dh5qqBAeWwz2Zc'
 		},
