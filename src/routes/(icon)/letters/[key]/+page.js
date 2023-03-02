4a2c4aa88bcda7c8949b71b9a617aa6531f999f6
mhousetree <mhousetree@gmail.com>
2023-03-02 17:34:50 +0900

    Use fetch function of load

--- src/routes/(icon)/letters/[key]/+page.js
+++ src/routes/(icon)/letters/[key]/+page.js
@@ -1,4 +1,4 @@
-export async function load({ params }) {
+export async function load({ params, fetch }) {
 	const response = await fetch(`https://okltrbackend-1-q1172794.deta.app/letters/${params.key}`
 	, {
 		method: "GET",
