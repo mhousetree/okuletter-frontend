2764ae96cb596bb9e1ee6685d4123ccc5c1a5224
mhousetree <mhousetree@gmail.com>
2023-03-02 15:55:08 +0900

    Add header

--- src/routes/(icon)/letters/[key]/+page.js
+++ src/routes/(icon)/letters/[key]/+page.js
@@ -1,5 +1,11 @@
 export async function load({ params }) {
-	const response = await fetch(`https://okltrbackend-1-q1172794.deta.app/letters/${params.key}`);
+	const response = await fetch(`https://okltrbackend-1-q1172794.deta.app/letters/${params.key}`
+	, {
+		headers: {
+			"X-Space-App-Key": 'HprcHEzprBLrjN8xu6Dh5qqBAeWwz2Zc'
+		}
+	}
+	);
 	return {
 		letter: response.ok && (await response.json())
 	};
